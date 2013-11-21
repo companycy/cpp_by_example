51a4300a1964657db4c8066503365451a9f1625c
companycy <companycy@gmail.com>
2013-11-22 06:38:15 +0800

    add disallow_copy_and_assign macro from google

--- singleton.cpp
+++ singleton.cpp
@@ -1,5 +1,21 @@
 #include <cstdio>
 
+// A macro to disallow the copy constructor and operator= functions
+// This should be used in the private: declarations for a class
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+
+class Foo {
+public:
+  Foo(int f);
+  ~Foo();
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(Foo);
+};
+
+
 class Singleton {
 public:
   static Singleton& getInst() {
